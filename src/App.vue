cab54f2ea22607189f5510741f711df2976a5b02
sadaaxy <1103901629@qq.com>
2021-03-25 16:31:32 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h2>HelloWorld</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
